489e93df6fede5a91c402fd7563fc2dd70f3cb2d
Aryan Gautam <161140375+Bootripper@users.noreply.github.com>
2024-03-01 15:35:40 +0530

    Add files via upload

---
+++ Task 7.py
@@ -0,0 +1,5 @@
+input=int(input("Please enter your number: "))
+if (input%2==0):
+    print("Even")
+else:
+    print("odd")                    
---
+++ Task 8.py
@@ -0,0 +1,5 @@
+input=int(input("Enter your Budget for car: "))
+if (input>=500000):
+    print("Purchase car")
+else:
+    print("Not purchase car")
