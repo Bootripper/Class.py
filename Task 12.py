d97e85be7e6918cf5bfbfb784e311e8a187c8b70
Aryan Gautam <161140375+Bootripper@users.noreply.github.com>
2024-03-02 14:29:22 +0530

    Task 12.py

--- Task 12.py
+++ Task 12.py
@@ -1,10 +1,13 @@
 #User input 
 input=int(input("Please enter Bill amount:"))
 if input>=5000:
-    print("You'll get 30% discount")
+    d=input*30/100
+    print("You'll get 30% discount: " , d)
 elif input>=2000 and input<=4999:
-    print("You'll get 20% discount")
+    d=input*20/100
+    print("You'll get 20% discount", d)
 elif input>=1000 and input<=1999:
-    print("You'll get 10% discount")
+    d=input*10/100
+    print("You'll get 10% discount", d)
 else:
-    print("Sorry, Try next time")
+    print("Add more to get more discount")
