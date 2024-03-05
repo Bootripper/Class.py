d131ca303f1b5c4ea59d0065e22e8d2a0eea9ecb
Aryan Gautam <161140375+Bootripper@users.noreply.github.com>
2024-03-05 16:22:12 +0530

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,7 @@
+user=int(input("Enter your Age: "))
+if (user<=18):
+    print("You are minor")
+elif(user>=18 and user<=65):
+    print("You are an Adult")
+else:
+    print("You are Senior Citizen")
---
+++ 189.py
@@ -0,0 +1,5 @@
+user=int(input("Enter your Age: "))
+if (user<=18):
+    print("You are minor")
+else:
+    print("You are an Adult")
