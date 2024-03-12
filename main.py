9c1f131363070e4b130879ecc2c85b6800b3f5bf
Aryan Gautam <161140375+Bootripper@users.noreply.github.com>
2024-03-12 16:58:59 +0530

    Def function

---
+++ main.py
@@ -0,0 +1,20 @@
+def number(n):
+  if(n%2==0):
+    print("Even")
+  else:
+    print("Odd")
+  print("Number", n)
+n1=2
+number(n1)
+
+n2=3
+number(n2)
+
+n3=4
+number(n3)
+
+n4=5
+number(n4)
+
+n5=9
+number(n5)
